0c368b3b9aa122446cd14030e88fb21544ef3b6b
DomKodukas <dominykas.pugacius@stud.svako.lt>
2022-04-06 15:09:35 +0300

    Add project files.

---
+++ uzkodavimas/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    public class AesOperation
+    {
+        public static string EncryptString(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            //PRIDETA EILUTE, KAD UZSIFRUOTAS KODAS NUKELIAUTU I SUKURTA FAILA
+            File.WriteAllText("uzkoduotas.txt", Convert.ToBase64String(array));
+            return Convert.ToBase64String(array);
+        }
+    }
+}
---
+++ uzkodavimas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "sdsasdawefasdfg90809as8df90a8sdf";
+
+            Console.WriteLine("Įveskite šifravimo eilutę:");
+            var str = Console.ReadLine();
+            var encryptedString = AesOperation.EncryptString(key, str);
+            Console.WriteLine($"Užšifruota eilutė = {encryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
